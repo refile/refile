49c42a55538109c768d37868eda10b595dff16d3
Janko Marohnić <janko.marohnic@gmail.com>
2014-12-27 11:04:11 +0100

    Put #set_expires_header into #stream_file

--- lib/refile/app.rb
+++ lib/refile/app.rb
@@ -22,27 +22,22 @@ module Refile
     end
 
     get "/:backend/:id/:filename" do
-      set_expires_header
       stream_file file
     end
 
     get "/:backend/:processor/:id/:file_basename.:extension" do
-      set_expires_header
       stream_file processor.call(file, format: params[:extension])
     end
 
     get "/:backend/:processor/:id/:filename" do
-      set_expires_header
       stream_file processor.call(file)
     end
 
     get "/:backend/:processor/*/:id/:file_basename.:extension" do
-      set_expires_header
       stream_file processor.call(file, *params[:splat].first.split("/"), format: params[:extension])
     end
 
     get "/:backend/:processor/*/:id/:filename" do
-      set_expires_header
       stream_file processor.call(file, *params[:splat].first.split("/"))
     end
 
@@ -75,15 +70,13 @@ module Refile
 
   private
 
-    def set_expires_header
-      expires Refile.content_max_age, :public, :must_revalidate
-    end
-
     def logger
       Refile.logger
     end
 
     def stream_file(file)
+      expires Refile.content_max_age, :public, :must_revalidate
+
       if file.respond_to?(:path)
         path = file.path
       else
