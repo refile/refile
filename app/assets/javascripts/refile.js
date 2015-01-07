65d544ccd26b8a0ead170fb2c5d4a4cd9b3a0a9b
Jonas Nicklas <jonas.nicklas@gmail.com>
2015-01-08 00:43:40 +0100

    Fix a couple of style issues in JS and make code clearer

--- app/assets/javascripts/refile.js
+++ app/assets/javascripts/refile.js
@@ -1,11 +1,11 @@
 (function() {
   "use strict";
 
-  if(!document.addEventListener) { return }; // IE8
+  if(!document.addEventListener) { return; } // IE8
 
-  document.addEventListener("change", function(e) {
-    if(e.target.tagName === "INPUT" && e.target.type === "file" && e.target.getAttribute("data-direct")) {
-      var input = e.target;
+  document.addEventListener("change", function(changeEvent) {
+    var input = changeEvent.target;
+    if(input.tagName === "INPUT" && input.type === "file" && input.getAttribute("data-direct")) {
       if(!input.files) { return; } // IE9, bail out if file API is not supported.
 
       var file = input.files[0];
@@ -15,13 +15,11 @@
         var ev = document.createEvent('CustomEvent');
         ev.initCustomEvent(name, true, false, detail);
         input.dispatchEvent(ev);
-      }
+      };
 
       if(file) {
-        var url = e.target.getAttribute("data-url");
-        if(e.target.getAttribute("data-fields")) {
-          var fields = JSON.parse(e.target.getAttribute("data-fields"));
-        }
+        var url = input.getAttribute("data-url");
+        var fields = JSON.parse(input.getAttribute("data-fields") || "null");
 
         var data = new FormData();
 
@@ -33,8 +31,8 @@
         data.append(input.getAttribute("data-as"), file);
 
         var xhr = new XMLHttpRequest();
-        xhr.addEventListener("load", function(e) {
-          input.classList.remove("uploading")
+        xhr.addEventListener("load", function() {
+          input.classList.remove("uploading");
           dispatchEvent("upload:complete", xhr.responseText);
           if((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
             var id = input.getAttribute("data-id") || JSON.parse(xhr.responseText).id;
@@ -48,16 +46,16 @@
           }
         });
 
-        xhr.upload.addEventListener("progress", function(e) {
-          if (e.lengthComputable) {
-            dispatchEvent("upload:progress", e);
+        xhr.upload.addEventListener("progress", function(progressEvent) {
+          if (progressEvent.lengthComputable) {
+            dispatchEvent("upload:progress", progressEvent);
           }
         });
 
         xhr.open("POST", url, true);
         xhr.send(data);
 
-        input.classList.add("uploading")
+        input.classList.add("uploading");
         dispatchEvent("upload:start", xhr);
       }
     }
