ab67b58ab4a48c007448237cf84b086992cde06a
Jonas Nicklas <jonas.nicklas@gmail.com>
2015-01-25 19:42:53 +0100

    Support Rails 3.2 via monkey patching, closes #117

--- lib/refile/rails.rb
+++ lib/refile/rails.rb
@@ -22,3 +22,18 @@ module Refile
     end
   end
 end
+
+# Add in missing methods for file uploads in Rails < 4
+ActionDispatch::Http::UploadedFile.class_eval do
+  unless instance_methods.include?(:eof?)
+    def eof?
+      @tempfile.eof?
+    end
+  end
+
+  unless instance_methods.include?(:close)
+    def close
+      @tempfile.close
+    end
+  end
+end
