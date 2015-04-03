7e382b4417dcdcc18cb9206b2c1377a72d112920
kobaltz <dave@k-innovations.net>
2015-04-03 13:49:20 -0400

    adding cacheonly

--- lib/refile.rb
+++ lib/refile.rb
@@ -313,6 +313,7 @@ Refile.configure do |config|
   config.logger = Logger.new(STDOUT)
   config.mount_point = "attachments"
   config.automount = true
+  config.cacheonly = false
   config.content_max_age = 60 * 60 * 24 * 365
   config.types[:image] = Refile::Type.new(:image, content_type: %w[image/jpeg image/gif image/png])
 end
