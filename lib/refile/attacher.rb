e4fe6406675f417ebb5a432cfe3a3c4288788ea0
Yasumasa Ashida <ys.ashida@gmail.com>
2021-01-27 20:10:43 +0900

    modify .rubocop.yml

--- lib/refile/attacher.rb
+++ lib/refile/attacher.rb
@@ -166,13 +166,13 @@ module Refile
 
   private
 
-    def read(column, strict= false)
+    def read(column, strict = false)
       m = "#{name}_#{column}"
       value ||= record.send(m) if strict or record.respond_to?(m)
       value
     end
 
-    def write(column, value, strict= false)
+    def write(column, value, strict = false)
       return if record.frozen?
 
       m = "#{name}_#{column}="
